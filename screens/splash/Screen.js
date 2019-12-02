5e8014207b4925f2e33f5c4ac8247469c974e839
josemak25 <amakirij@gmail.com>
2019-12-02 20:43:01 +0100

    add screen component for splash screen rendering

---
+++ screens/splash/Screen.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import {
+  Container,
+  Background,
+  SplashContent,
+  SplashContentFooter,
+  SplashContentHeader,
+  BackgroundImage
+} from "./styles";
+
+import appTheme from "../../constants";
+
+export default function Screen({ item }) {
+  const [theme] = appTheme;
+
+  return (
+    <Container>
+      <Background>
+        <BackgroundImage source={item.image} resizeMode="contain" />
+        <SplashContent>
+          <SplashContentHeader
+            color={theme.PRIMARY_COLOR}
+            font={theme.FONT_FAMILY_REGULAR}
+          >
+            {item.title}
+          </SplashContentHeader>
+          <SplashContentFooter
+            color={theme.PRIMARY_COLOR}
+            font={theme.FONT_FAMILY_REGULAR}
+          >
+            {item.text}
+          </SplashContentFooter>
+        </SplashContent>
+      </Background>
+    </Container>
+  );
+}
