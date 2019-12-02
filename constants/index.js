6d7d88649a03247d12ae11a45da3ac476f6daddf
josemak25 <amakirij@gmail.com>
2019-12-02 20:24:18 +0100

    add app constants like colors and app theme

---
+++ constants/index.js
@@ -0,0 +1,53 @@
+// All app colors
+export const colors = {
+  FONT_SIZE_SMALL: 12,
+  FONT_SIZE_MEDIUM: 14,
+  FONT_SIZE_LARGE: 16,
+  PRIMARY_COLOR: "rgba(28,122,151,1)",
+  SECONDARY_COLOR: "rgb(42, 50, 85)",
+  SHADOW_COLOR: "rgba(67,189,210,1)",
+  SVG_COLOR: "rgba(103,202,218,1)",
+  COLOR_VARIANT_ONE: "rgb(46, 182, 206)",
+  COLOR_VARIANT_TWO: "rgb(67, 189, 209)",
+  COLOR_VARIANT_THREE: "rgb(103, 202, 218)",
+  COLOR_VARIANT_FOUR: "rgb(0, 180, 212)",
+  FONT_FAMILY_BOLD: "panton-bold",
+  FONT_FAMILY_REGULAR: "panton-regular",
+  FONT_FAMILY_LIGHT: "panton-light",
+  FONT_WEIGHT_LIGHT: 200,
+  FONT_WEIGHT_MEDIUM: 600,
+  FONT_WEIGHT_HEAVY: 800
+};
+
+// App theme
+export default theme = [
+  {
+    FONT_SIZE_SMALL: 12,
+    FONT_SIZE_MEDIUM: 14,
+    FONT_SIZE_LARGE: 16,
+    PRIMARY_COLOR: "rgba(28,122,151,1)",
+    SECONDARY_COLOR: "rgb(42, 50, 85)",
+    SHADOW_COLOR: "rgba(67,189,210,1)",
+    SVG_COLOR: "rgba(103,202,218,1)",
+    COLOR_VARIANT_ONE: "rgb(46, 182, 206)",
+    COLOR_VARIANT_TWO: "rgb(67, 189, 209)",
+    COLOR_VARIANT_THREE: "rgb(103, 202, 218)",
+    COLOR_VARIANT_FOUR: "rgb(0, 180, 212)",
+    FONT_FAMILY_BOLD: "panton-bold",
+    FONT_FAMILY_REGULAR: "panton-regular",
+    FONT_FAMILY_LIGHT: "panton-light",
+    FONT_WEIGHT_LIGHT: 200,
+    FONT_WEIGHT_MEDIUM: 600,
+    FONT_WEIGHT_HEAVY: 800
+  },
+  {
+    FONT_SIZE_SMALL: 12,
+    FONT_SIZE_MEDIUM: 14,
+    FONT_SIZE_LARGE: 16,
+    PRIMARY_COLOR: "rgb(30, 147, 242)",
+    SECONDARY_COLOR: "rgb(238, 167, 2)",
+    FONT_WEIGHT_LIGHT: 200,
+    FONT_WEIGHT_MEDIUM: 600,
+    FONT_WEIGHT_HEAVY: 800
+  }
+];
