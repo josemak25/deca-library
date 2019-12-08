7a7cd50616b89c585386fc210a72e0240a795eaa
josemak25 <amakirij@gmail.com>
2019-12-08 20:17:44 +0100

    fix style for card

--- components/card/styles.js
+++ components/card/styles.js
@@ -3,8 +3,8 @@ import theme from "../../constants";
 const [colors] = theme;
 
 export const Container = styled.TouchableOpacity`
-  min-width: 100%;
-  min-height: 160;
+  width: 100%;
+  height: 160;
   justify-content: center;
   align-items: center;
   background-color: ${colors.COLOR_WHITE};
