2307b6b6d5b4573e2e4560ed460619ff99c230b4
josemak25 <amakirij@gmail.com>
2019-12-09 11:48:01 +0100

    add book carousel card to home component

--- screens/home/index.js
+++ screens/home/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
-import { Container, Header } from "./styles";
+import { Container, Header, Background, CarouselContainer } from "./styles";
 import components from "../../components";
 
-const { Card, Loading, Book } = components;
+const { Card, Loading, Book, CarouselItem } = components;
+
 import getBooks from "../../redux/actions/booksActions";
 
 const Home = props => {
@@ -24,20 +25,40 @@ const Home = props => {
     <Loading />
   ) : (
     <Container>
-      <Header>Discover</Header>
+      <Background>
+        <Header>Discover</Header>
 
-      {/* scrolling carousel here */}
+        {/* scrolling carousel here */}
+        <CarouselContainer>
+          <CarouselItem
+            item={{
+              title: "dry",
+              category: "drama",
+              author: "Neal Shusterman",
+              description:
+                "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur",
+              rating: "1004",
+              imageLinks: {
+                thumbnail:
+                  "http://books.google.com/books/content?id=zyTCAlFPjgYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73E8MHnwP1HoqWu3Z8F2OMWLreXQlfZYMlw1Q4fD3Slt_qf8GqcjrjuNHmgk3L4eFdRN8JWU2Q_bexRlxiSV7qLDfaLk0I79FP9aYNu0XX85_QuaVIS0ba6xeOX_z1LCxGpxomJ&source=gbs_api"
+              }
+            }}
+          />
+        </CarouselContainer>
 
-      <Card>
-        <Book
-          {...{
-            title: "dry",
-            category: "drama",
-            author: "Neal Shusterman",
-            rating: "1004"
-          }}
-        />
-      </Card>
+        {/* scrolling carousel here */}
+
+        <Card>
+          <Book
+            {...{
+              title: "dry",
+              category: "drama",
+              author: "Neal Shusterman",
+              rating: "1004"
+            }}
+          />
+        </Card>
+      </Background>
     </Container>
   );
 };
