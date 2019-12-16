b2cfc3581103b612c4d0b5bc11cf4a43f4f5fd2b
josemak25 <amakirij@gmail.com>
2019-12-16 13:14:48 +0100

    add image carousel background with details

--- screens/home/index.js
+++ screens/home/index.js
@@ -5,12 +5,22 @@ import Carousel from "react-native-snap-carousel";
 import components from "../../components";
 const { Card, Loading, Book, CarouselItem } = components;
 
-import { Container, Header, Background, CarouselContainer } from "./styles";
+import {
+  Container,
+  Header,
+  Background,
+  CarouselContainer,
+  Body,
+  Category,
+  Title,
+  Description,
+  carouselBackgroundStyle
+} from "./styles";
 
 const { width, height } = Dimensions.get("window");
 
 const CONTENT_WIDTH = width - 40;
-const CONTENT_HEIGHT = height * CONTENT_WIDTH;
+const CONTENT_HEIGHT = height / 7;
 
 import jsonBooks from "../../books.json";
 
@@ -23,12 +33,26 @@ const Home = props => {
     error: null
   });
 
+  const [curScrollItem, setCurScrollItem] = useState({
+    book: jsonBooks.items[0].volumeInfo,
+    bookIndex: 0
+  });
+
   useEffect(() => {
     // props.dispatch(getBooks());
     const { books, isLoading, error } = props.books;
 
     setLibrary({ ...library, books, isLoading, error });
   }, []);
+
+  const handleScroll = index => {
+    console.log(index);
+    setCurScrollItem({
+      ...curScrollItem,
+      bookIndex: index,
+      book: jsonBooks.items[index].volumeInfo
+    });
+  };
 
   return library.isLoading ? (
     <Loading />
@@ -42,23 +66,38 @@ const Home = props => {
           <Carousel
             data={jsonBooks.items}
             renderItem={CarouselItem}
-            sliderWidth={CONTENT_WIDTH}
+            sliderWidth={width}
             itemWidth={CONTENT_WIDTH}
-            itemHeight={CONTENT_HEIGHT}
             sliderHeight={CONTENT_HEIGHT}
+            layoutCardOffset={18}
+            inactiveSlideOpacity={1}
+            inactiveSlideScale={1}
+            onSnapToItem={handleScroll}
           />
         </CarouselContainer>
-
         {/* scrolling carousel here */}
 
+        <Card styles={carouselBackgroundStyle}>
+          <Body>
+            <Category>{curScrollItem.book.categories}</Category>
+            <Title>{curScrollItem.book.title}</Title>
+            <Description>
+              {curScrollItem.book.description
+                ? curScrollItem.book.description.substr(0, 50)
+                : curScrollItem.book.subtitle}
+              ...
+            </Description>
+          </Body>
+        </Card>
+
         <Card>
-          <Book book={jsonBooks.items[23]} />
+          <Book book={jsonBooks.items[0]} />
         </Card>
       </Background>
     </Container>
   );
 };
 
-const mapStateToProps = state => ({ books: state.books });
+const mapStateToProps = ({ books }) => ({ books });
 
 export default connect(mapStateToProps)(Home);
