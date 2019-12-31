0191a4ede4685863b9d2f3cf67e4a83585e588af
josemak25 <amakirij@gmail.com>
2019-12-31 11:00:29 +0100

    add fix to icon props

--- components/icon/index.js
+++ components/icon/index.js
@@ -1,6 +1,4 @@
 import React from "react";
 import Icon from "react-native-vector-icons/Ionicons";
 
-export default IconContainer = ({ name, size, styles }) => {
-  return <Icon name={name} size={size} style={styles} />;
-};
+export default IconContainer = props => <Icon {...props} />;
